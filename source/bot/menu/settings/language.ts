9b2585861ffd5427ad0ef37d63e00fef1db4a746
EdJoPaTo <git-commit-with-a-funny-long-email-address-everyone-hates-as-it-is-too-long@edjopato.de>
2020-11-08 13:23:58 +0100

    fix(languages): load availableLocales at startup

--- source/bot/menu/settings/language.ts
+++ source/bot/menu/settings/language.ts
@@ -1,13 +1,15 @@
 import {MenuTemplate} from 'telegraf-inline-menu';
+import {I18n} from '@edjopato/telegraf-i18n';
 
 import {MyContext} from '../../my-context';
 
 import {backButtons} from '../general';
 
+const availableLocales = new I18n({directory: 'locales'}).availableLocales();
+
 export const menu = new MenuTemplate<MyContext>(context => context.i18n.t('settings.language'));
 
-// TODO: find out why context.i18n.availableLocales() doesnt work
-menu.select('lang', ['en', 'de'], {
+menu.select('lang', availableLocales, {
 	isSet: (context, key) => context.i18n.locale() === key,
 	set: (context, key) => {
 		context.i18n.locale(key);
