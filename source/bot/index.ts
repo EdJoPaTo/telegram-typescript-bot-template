ff906c50b15de72381a37c2f7560648f2fd4e6de
EdJoPaTo <git-commit-with-a-funny-long-email-address-everyone-hates-as-it-is-too-long@edjopato.de>
2020-11-08 13:22:06 +0100

    refactor: disable esModuleInterop
    personal opinion: keep it off when not needed.
    Keep the config as close as possible to the extended one -> simpler to understand

--- source/bot/index.ts
+++ source/bot/index.ts
@@ -3,7 +3,7 @@ import {existsSync, readFileSync} from 'fs';
 import {I18n as TelegrafI18n} from '@edjopato/telegraf-i18n';
 import {MenuMiddleware} from 'telegraf-inline-menu';
 import {Telegraf} from 'telegraf';
-import TelegrafSessionLocal from 'telegraf-session-local';
+import * as TelegrafSessionLocal from 'telegraf-session-local';
 
 import {fightDragons, danceWithFairies} from '../magic';
 
