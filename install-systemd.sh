e40c6978053f6d0e26e1abda23b3076bb385bc6f
EdJoPaTo <git-commit-with-a-funny-long-email-address-everyone-hates-as-it-is-too-long@edjopato.de>
2021-02-15 16:37:41 +0100

    chore(systemd): add systemd template

---
+++ install-systemd.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+nice npm ci --production
+nice npm run build
+
+echo
+echo WARNING
+echo Service will fail when some values are not filled out
+echo
+
+# systemd
+sudo cp -uv *.service /etc/systemd/system
+sudo systemctl daemon-reload
+
+# start
+sudo systemctl restart telegram-typescript-bot-template.service
+sudo systemctl enable telegram-typescript-bot-template.service
